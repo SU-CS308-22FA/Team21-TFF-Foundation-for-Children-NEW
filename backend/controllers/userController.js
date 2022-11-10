e6ea7bbf3d95d59e1fa119f8340694ee3bcfd50d
cerenarkac21 <88002530+cerenarkac21@users.noreply.github.com>
2022-11-10 22:36:39 +0300

    modified:   backend/controllers/userController.js 	modified:   backend/models/userModel.js 	modified:   frontend/src/index.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	deleted:    frontend/src/App.css 	modified:   frontend/src/App.js 	deleted:    frontend/src/App.test.js 	deleted:    frontend/src/logo.svg 	deleted:    frontend/src/reportWebVitals.js 	deleted:    frontend/src/setupTests.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,14 @@
 const User = require('../models/userModel')
-//const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-/*
-const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-}
-*/
 
-// login a user
-const loginUser = async (req, res) => { // async function bc it will communicate with the db
-    res.json({mssg: "login user"})
-}
-
-// sign up a user
-const signupUser = async (req, res) => { // async function bc it will communicate with the db
-    res.json({mssg: "signup user"})
+const createToken = (_id) => { // after the tokens are created, 
+//they will be used in loginUser and signupUser controller functions.
+//_id will be a part of the payload of the token.
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' }) // user stays logged in for 3 days. then the token expires.
 }
 
 
-/*
 const loginUser = async (req, res) => { // async function bc it will communicate with the db
   const {email, password} = req.body
 
@@ -36,7 +26,7 @@ const loginUser = async (req, res) => { // async function bc it will communicate
 
 // signup a user
 const signupUser = async (req, res) => {
-  const {email, password} = req.body
+  const {email, password} = req.body // pull the email and password of the user from req. and name them email and password
 
   try {
     const user = await User.signup(email, password)
@@ -45,10 +35,10 @@ const signupUser = async (req, res) => {
     const token = createToken(user._id)
 
     res.status(200).json({email, token})
-  } catch (error) {
+  } catch (error) { // error is coming from userModel.js. or if it is related to creating db, mongodb can also give an error message.
     res.status(400).json({error: error.message})
   }
 }
 
-*/
+
 module.exports = { signupUser, loginUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,8 +2,8 @@
 // mongoose will create the models and schemas 
 //for our data in the mongodb, because mongodb alone is schema-less.
 const mongoose = require('mongoose')  
-//const bcrypt = require('bcrypt')
-//const validator = require('validator')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -18,17 +18,23 @@ const userSchema = new Schema({
     required: true
   }
 })
-/*
+
 // make a model based on the schema
 // schema defines the stucture of a type of document inside our db.
 // model applies that schema to a particular model
 
 // static signup method
+/*
+A static method (or static function) is a method defined as a member of an object 
+but is accessible directly from an API object's constructor, 
+rather than from an object instance created via the constructor.
+*/
 userSchema.statics.signup = async function(email, password) {  // create a function name with signup 
+// bc we are using this keyword, we cannot use arrow func. we need to use asyncrh. regular function.
 
   // validation
   if (!email || !password) {
-    throw Error('All fields must be filled')
+    throw Error('All fields must be filled')  // thrown errors will be catched in userController.js
   }
   if (!validator.isEmail(email)) {
     throw Error('Email not valid')
@@ -37,16 +43,17 @@ userSchema.statics.signup = async function(email, password) {  // create a funct
     throw Error('Password not strong enough')
   }
 
-  const exists = await this.findOne({ email })
+  const exists = await this.findOne({ email }) // check if a email is given by the user.
+//because we created a static method, instead of using the model nanme(User), use this. it refers to the model
 
   if (exists) {
     throw Error('Email already in use')
   }
 
-  const salt = await bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10) //in order to add additional characters to the passwords for security purposes.
   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ email, password: hash })
+  const user = await this.create({ email, password: hash }) // swap password and hashed password
 
   return user
 }
@@ -54,7 +61,7 @@ userSchema.statics.signup = async function(email, password) {  // create a funct
 // static login method
 userSchema.statics.login = async function(email, password) {
 
-  if (!email || !password) {
+  if (!email || !password) { // password is the plain text password
     throw Error('All fields must be filled')
   }
 
@@ -63,7 +70,7 @@ userSchema.statics.login = async function(email, password) {
     throw Error('Incorrect email')
   }
 
-  const match = await bcrypt.compare(password, user.password)
+  const match = await bcrypt.compare(password, user.password) // user.password is the hashed password
   if (!match) {
     throw Error('Incorrect password')
   }
@@ -71,7 +78,6 @@ userSchema.statics.login = async function(email, password) {
   return user
 }
 
-*/
 
 module.exports = mongoose.model('User', userSchema)
 // User model was exported. I will use that to interact with the users collection.
