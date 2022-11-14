70baba426eb9ba382645e33d0199b1554c2f10f3
AliFehmi <101139947+AliFehmi@users.noreply.github.com>
2022-11-14 12:31:20 +0300

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.js 	new file:   frontend/src/components/Navbar.js 	new file:   frontend/src/context/AuthContext.js 	new file:   frontend/src/hooks/useAuthContext.js 	new file:   frontend/src/hooks/useLogin.js 	new file:   frontend/src/hooks/useLogout.js 	new file:   frontend/src/hooks/useSignup.js 	modified:   frontend/src/index.css 	modified:   frontend/src/index.js 	new file:   frontend/src/pages/Login.js 	new file:   frontend/src/pages/Signup.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
