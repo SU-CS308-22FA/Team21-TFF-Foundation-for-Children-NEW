6df6c995272ec88072652a8ed54cc5c97ee471c0
AliFehmi <101139947+AliFehmi@users.noreply.github.com>
2022-11-21 15:33:55 +0300

    new file:   frontend/img/1.jpg 	new file:   frontend/img/1370177783.jpg 	new file:   frontend/img/2.jpg 	new file:   frontend/img/3.jpg 	new file:   frontend/img/4.jpg 	new file:   frontend/img/40bf1de7-621d-4529-a98d-4f4ae5b8b711.jfif 	new file:   frontend/img/5.jpg 	new file:   frontend/img/510026.jpg 	new file:   frontend/img/6.jpg 	new file:   frontend/img/mt-0966-logo.png 	modified:   frontend/src/index.css 	modified:   frontend/src/pages/Student.js

--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -9,99 +9,124 @@ const Student= () => {
 
 
 
-            <div className="profileContainer">
-                <div id="userBox">
-                    <h3>user name</h3>
-                </div>
-                <div id="profileBox">
-                    <h3>My Profile</h3>
-                </div>
-                <div id="logoutBox">
-                    <h3>Log out</h3>
-                </div>
-            </div>
-            <div className="personalDataBox">
-                <div id="announcementBox">
-                    <h3>Announcements</h3>
-                </div>
-                <div id="eventBox">
-                    <h3>Events</h3>
-                </div>
-                <div id="developmentBox">
-                    <h3>My Developments</h3>
-                </div>
-            </div> 
-            <div className="informationContainer">
-                <div id="infoBox">
-                    <h1> HEADER 1 </h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum consequatur placeat hic similique accusantium aut excepturi impedit consequuntur est aliquam ullam nam dicta magnam, laudantium, ea quia. Quos, ipsam omnis.</p>
-                </div>
-                <div id="infoBox">
-                    <h1> HEADER 2 </h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum consequatur placeat hic similique accusantium aut excepturi impedit consequuntur est aliquam ullam nam dicta magnam, laudantium, ea quia. Quos, ipsam omnis.</p>
-                </div>
-                <div id="infoBox">
-                    <h1> HEADER 3 </h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum consequatur placeat hic similique accusantium aut excepturi impedit consequuntur est aliquam ullam nam dicta magnam, laudantium, ea quia. Quos, ipsam omnis.</p>
-                </div>
-            </div>
-            <div className="calendar">
-                <div className="month">      
-                    <ul>
-                    <li className="prev">&#10094;</li>
-                    <li className="next">&#10095;</li>
-                    <li>
-                        August<br/>
-                        <span>2021</span>
-                    </li>
-                    </ul>
-                </div>
-                
-                <ul className="weekdays">
-                    <li>Mo</li>
-                    <li>Tu</li>
-                    <li>We</li>
-                    <li>Th</li>
-                    <li>Fr</li>
-                    <li>Sa</li>
-                    <li>Su</li>
-                </ul>
-                
-                <ul className="days">  
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                    <li>9</li>
-                    <li><span className="active">10</span></li>
-                    <li>11</li>
-                    <li>12</li>
-                    <li>13</li>
-                    <li>14</li>
-                    <li>15</li>
-                    <li>16</li>
-                    <li>17</li>
-                    <li>18</li>
-                    <li>19</li>
-                    <li>20</li>
-                    <li>21</li>
-                    <li>22</li>
-                    <li>23</li>
-                    <li>24</li>
-                    <li>25</li>
-                    <li>26</li>
-                    <li>27</li>
-                    <li>28</li>
-                    <li>29</li>
-                    <li>30</li>
-                    <li>31</li>
-                </ul>
-            </div>
+            <div class="introImage">
+                        <div class="nav">
+                            <img src="./img/mt-0966-logo.png" />
+                            <div id="home"> Home </div>
+                            <div id="about"> About </div>
+                            <div id="announcements">Announcements </div>
+                            <div id="events"> Events </div>
+                            <div id="development"> My Development </div>
+                        </div>
+                    </div>
+                    <div class="aboutDescription">
+                        
+                        <div class="infoBox">
+                            <h1> About Header 1 </h1> 
+                            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus deleniti magnam cupiditate odit minus accusantium dignissimos velit quas. Accusantium, id voluptatibus assumenda adipisci aliquam ut? Commodi necessitatibus blanditiis error. </p>
+                        </div>
+                        <div class="infoBox">
+                            <h1> About Header 2 </h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt iure vero animi, possimus asperiores ipsa quasi fuga. Ab nemo esse aliquam autem! Veritatis dolores quia ex, est perferendis soluta quaerat!</p>
+                        </div>
+                        <div class="infoBox">
+                            <h1> About Header 3 </h1>
+                            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo fuga impedit dolorem commodi nisi. Illo ducimus nihil doloribus quasi facilis eaque tenetur maxime laboriosam ad, in, repellendus aliquam at obcaecati.</p>
+                        </div>
+                    </div>
+                    <div class="purposeDescription">
+                        <img src="./img/1370177783.jpg" />
+                        <div class="description">
+                            <h1> Purpose Header 1 </h1> 
+                            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus deleniti magnam cupiditate odit minus accusantium dignissimos velit quas. Accusantium, id voluptatibus assumenda adipisci aliquam ut? Commodi necessitatibus blanditiis error. </p>
+                        </div>
+                    </div>
 
+                    <div class="skillContainer">
+                        <div class="skills">
+                            <h1>My Development</h1>
+                            <div id="img1">
+                                <h3> Skill Name 1</h3>
+                                <img src="../"/>
+                            </div>
+                            <div id="img2">
+                                <h3> Skill Name 2</h3>
+                                <img src="img/2.jpg"/>
+                            </div>
+                            <div id="img3">
+                                <h3> Skill Name 3</h3>
+                                <img src="img/3.jpg"/>
+                            </div>
+                            <div id="img4">
+                                <h3> Skill Name 4</h3>
+                                <img src="img/4.jpg"/>
+                            </div>
+                            <div id="img5">
+                                <h3> Skill Name 5</h3>
+                                <img src="img/5.jpg"/>
+                            </div>
+                            <div id="img6">
+                                <h3> Skill Name 6</h3>
+                                <img src="img/6.jpg"/>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    <div class="announcementsContainer">
+        
+            
+                    <div class="announcements">
+                        <h1> Latest News </h1>
+                        <div class="news">
+                            <div class="announcementBox1">
+                                <img src="img/4.jpg"/>
+                                <div class="eventDate1">
+                                    <h4>April 26, 2022</h4>
+                                </div>
+                                <div class="eventName1">
+                                    <h2>Soccer Open Day</h2>
+                                </div>
+                                <div class="eventDescription1">
+                                    <p>
+                                        It is vitally important that the club retains its existing players and attracts new players each season. 
+                                        From early July the club will arrange a Club Open Day ...
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="announcementBox2">
+                                <img src="img/5.jpg"/>
+                                <div class="eventDate2">
+                                    <h4>April 26, 2022</h4>
+                                </div>
+                                <div class="eventName2">
+                                    <h2>FAI Summer Camp</h2>
+                                </div>
+                                <div class="eventDescription2">
+                                    <p>
+                                        The Football Association of Ireland runs skills-based summer camps at many soccer clubs throughout 
+                                        the country during the summer period.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="announcementBox3">
+                                <img src="img/1.jpg"/>
+                                <div class="eventDate3">
+                                    <h4>April 26, 2022</h4>
+                                </div>
+                                <div class="eventName3">
+                                    <h2>Trip to Premiership Match</h2>
+                                </div>
+                                <div class="eventDescription3">
+                                    <p>
+                                        Each season a group of players, parents & 
+                                        coaches travel across to the UK to watch a premiership match. A guided tour of the home stadium is sometimes...
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
 
         
