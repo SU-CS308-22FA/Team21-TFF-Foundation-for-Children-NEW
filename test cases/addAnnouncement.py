4d3239d56a8ea4570277dcdd4da0e16319bdb956
FluffyGazelle <mtunahan@sabanciuniv.edu>
2023-01-04 00:44:36 +0300

    Tunahan+Zeynep's test cases

---
+++ test cases/addAnnouncement.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome("/chromedriver.exe") 
+driver.get("http://localhost:3000/")
+time.sleep(2)
+
+
+toLogin=WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "Login")))
+toLogin.click()
+time.sleep(2)
+
+emailInput = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/input[1]')
+emailInput.send_keys('ogretmen@ogretmen.com')
+passwordInput = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/input[2]')
+passwordInput.send_keys('123abc!ABC')
+driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/button').click()
+time.sleep(1)
+driver.find_element(By.XPATH, '/html/body/div/div/div/switch/div/div[2]/a[2]').click()
+time.sleep(0.5)
+title=driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/input')
+title.send_keys("Deneme Test to Announcement  Title")
+content=driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/textarea')
+content.send_keys("Deneme Test to Announcement Content")
+driver.find_element(By.XPATH, '//*[@id="root"]/div/div/switch/div/form/button').click()
+
+
